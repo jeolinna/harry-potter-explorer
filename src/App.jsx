b03c76b12d7b40dce534f8f881a9c2a0e5696151
alina <kostenkoalina97@icloud.com>
2025-11-26 12:29:59 +0100

    chore: set up GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CharactersPage from "./pages/CharactersPage";
 import CharacterDetailsPage from "./pages/CharacterDetails";
 import SpellsPage from "./pages/SpellsPage";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/characters",
-      element: <CharactersPage />,
-    },
-    {
-      path: "/characters/:id",
-      element: <CharacterDetailsPage />,
-    },
-    {
-      path: "/spells",
-      element: <SpellsPage />,
-    },
-  ],
-  {
-    basename: import.meta.env.BASE_URL,
-  }
-);
+const router = createHashRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/characters", element: <CharactersPage /> },
+  { path: "/characters/:id", element: <CharacterDetailsPage /> },
+  { path: "/spells", element: <SpellsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
