d092fcc08a535ab4d59fd6d1808084fde9d41434
alina <kostenkoalina97@icloud.com>
2025-11-26 01:21:47 +0100

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CharactersPage from "./pages/CharactersPage";
+import CharacterDetailsPage from "./pages/CharacterDetails";
+import SpellsPage from "./pages/SpellsPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/characters",
+    element: <CharactersPage />,
+  },
+  {
+    path: "/characters/:id",
+    element: <CharacterDetailsPage />,
+  },
+  {
+    path: "/spells",
+    element: <SpellsPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <h1 className="text-4xl font-bold text-red-500 text-end">Hello</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
